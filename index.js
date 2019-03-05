27263465bb105e89eb6323bdcdb1149df8cb8884
ayuli <649160717@qq.com>
2019-03-05 09:09:52 +0800

    debug

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ io.on('connection', function(socket){
     });
 
     socket.on('chat message', function(msg){
+        console.log('chat message');
         io.emit('chat message', msg);
     });
 });
